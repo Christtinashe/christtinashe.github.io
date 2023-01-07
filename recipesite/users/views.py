b447c9b533d7807f1032beb54410806204a949c2
Christtinashe <111988297+Christtinashe@users.noreply.github.com>
2023-01-07 18:46:53 +0200

    Add files via upload

---
+++ recipesite/users/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from . import forms
+from django.contrib.auth.decorators import login_required
+#Create your views here.
+
+def register(request):
+    if request.method=="POST":
+       form = forms.UserRegisterForm(request.POST)
+       if form.is_valid():
+        username= form.cleaned_data.get('username')
+        messages.success(request, f"{username}, Your account has been created, Please login")
+        return redirect('user-login')
+    else:
+        form = forms.UserRegisterForm()
+    return render(request, 'users/register.html', {'form': form})
+
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')
